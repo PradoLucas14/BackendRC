e9fe846d7e1fbcccd778d81db09e1a4459f1795e
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2024-07-03 19:52:25 -0300

    se creo la condicional en el registro de usuarios para que no se permita registrar dos usuarios con el mismo email

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,29 +3,35 @@ const bcrypt = require('bcrypt');
 
 // Función para registrar un nuevo usuario
 const registerUser = async (req, res) => {
-  try {
-    const { username, email, password, termsAccepted, role, accountActive } = req.body;
-
-    // Crear una nueva instancia del modelo User
-    const newUser = new User({
-      username,
-      email,
-      password, // La contraseña será encriptada automáticamente
-      termsAccepted,
-      role,
-      accountActive
-    });
-
-    // Guardar el nuevo usuario en la base de datos
-    await newUser.save();
-
-    // Responder con éxito
-    res.status(201).json({ message: 'Usuario registrado exitosamente', user: newUser });
-  } catch (error) {
-    console.error('Error al registrar el usuario:', error);
-    res.status(500).json({ message: 'Error al registrar el usuario', error });
-  }
-};
+    try {
+      const { username, email, password, termsAccepted, role, accountActive } = req.body;
+  
+      // Verificar si el correo electrónico ya está en uso
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ message: 'El correo electrónico ya está en uso' });
+      }
+  
+      // Crear una nueva instancia del modelo User
+      const newUser = new User({
+        username,
+        email,
+        password, // La contraseña será encriptada automáticamente
+        termsAccepted,
+        role,
+        accountActive
+      });
+  
+      // Guardar el nuevo usuario en la base de datos
+      await newUser.save();
+  
+      // Responder con éxito
+      res.status(201).json({ message: 'Usuario registrado exitosamente', user: newUser });
+    } catch (error) {
+      console.error('Error al registrar el usuario:', error);
+      res.status(500).json({ message: 'Error al registrar el usuario', error });
+    }
+  };
 
 // Función para obtener todos los usuarios
 const getUsers = async (req, res) => {
