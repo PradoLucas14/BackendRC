0fab6c4c33ba8a0cd8250ddb02e32e8a2558ea28
MatiasPerez00 <matiasperez.personal@gmail.com>
2024-07-04 16:09:15 -0300

    Cambios en la verificacion

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -48,6 +48,9 @@ const verifyCode = async (request, response) => {
   }
 };
 
+
+
+
 // Registrar un nuevo usuario
 const registerUser = async (request, response) => {
   try {
@@ -70,12 +73,81 @@ const registerUser = async (request, response) => {
   }
 };
 
+// Iniciar sesión
+const loginUser = async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    // Buscar el usuario por correo electrónico
+    const user = await User.findOne({ email });
+    if (!user) {
+      return response.status(401).json({ message: 'Correo electrónico' });
+    }
+
+    // Verificar la contraseña
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return response.status(401).json({ message: 'Contraseña incorrectos' });
+    }
+
+    // Crear el token JWT
+    const token = jwt.sign({ userId: user._id,userRole: user.role }, process.env.JWT_SECRET, { expiresIn: '30m' });
+
+    response.status(200).json({ message: 'Login exitoso', accesToken:token });
+  } catch (error) {
+    response.status(500).json({ message: 'Error al iniciar sesión', error: error.message });
+  }
+};
+
+// Obtener todos los usuarios
+const getUsers = async (request, response) => {
+  try {
+    const users = await User.find();
+    response.status(200).json(users);
+  } catch (error) {
+    response.status(500).json({ message: 'Error al obtener los usuarios', error: error.message });
+  }
+};
+
+// Eliminar un usuario
+const deleteUser = async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    // Eliminar el usuario por ID
+    const deletedUser = await User.findByIdAndDelete(id);
+    if (!deletedUser) {
+      return response.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    response.status(200).json({ message: 'Usuario eliminado con éxito' });
+  } catch (error) {
+    response.status(500).json({ message: 'Error al eliminar el usuario', error: error.message });
+  }
+};
+
+// Actualizar un usuario
+const updateUser = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const { username, accountActive } = request.body;
+
+    // Actualizar el usuario por ID
+    const updatedUser = await User.findByIdAndUpdate(id, { username, accountActive }, { new: true });
+    if (!updatedUser) {
+      return response.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    response.status(200).json({ message: 'Usuario actualizado con éxito', user: updatedUser });
+  } catch (error) {
+    response.status(500).json({ message: 'Error al actualizar el usuario', error: error.message });
+  }
+};
+
 module.exports = {
   registerUser,
   loginUser,
   getUsers,
   deleteUser,
-  updateUser,
-  sendVerificationCode,
-  verifyCode
+  updateUser
 };
