da64ed916bf5f02c8fe3d3916939cb2893ed3efe
MatiasPerez00 <matiasperez.personal@gmail.com>
2024-07-04 00:50:16 -0300

    cambios de datos enviados en json

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,16 +1,16 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
+require('dotenv').config()
 // Registrar un nuevo usuario
-const registerUser = async (req, res) => {
+const registerUser = async (request, response) => {
   try {
-    const { username, email, password, termsAccepted, role, accountActive } = req.body;
+    const { username, email, password, termsAccepted, role, accountActive } = request.body;
 
     // Verificar si el correo electrónico ya está en uso
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: 'El correo electrónico ya está en uso' });
+      return response.status(400).json({ message: 'El correo electrónico ya está en uso' });
     }
 
     // Crear un nuevo usuario
@@ -18,80 +18,80 @@ const registerUser = async (req, res) => {
 
     // Guardar el nuevo usuario en la base de datos
     await newUser.save();
-    res.status(201).json({ message: 'Usuario registrado con éxito', user: { username, email, role, accountActive } });
+    response.status(201).json({ message: 'Usuario registrado con éxito', user: { username, email, role, accountActive } });
   } catch (error) {
-    res.status(500).json({ message: 'Error al registrar el usuario', error: error.message });
+    response.status(500).json({ message: 'Error al registrar el usuario', error: error.message });
   }
 };
 
 // Iniciar sesión
-const loginUser = async (req, res) => {
+const loginUser = async (request, response) => {
   try {
-    const { email, password } = req.body;
+    const { email, password } = request.body;
 
     // Buscar el usuario por correo electrónico
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(401).json({ message: 'Correo electrónico o contraseña incorrectos' });
+      return response.status(401).json({ message: 'Correo electrónico' });
     }
 
     // Verificar la contraseña
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(401).json({ message: 'Correo electrónico o contraseña incorrectos' });
+      return response.status(401).json({ message: 'Contraseña incorrectos' });
     }
 
     // Crear el token JWT
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '30m' });
 
-    res.status(200).json({ message: 'Login exitoso', token });
+    response.status(200).json({ message: 'Login exitoso', token });
   } catch (error) {
-    res.status(500).json({ message: 'Error al iniciar sesión', error: error.message });
+    response.status(500).json({ message: 'Error al iniciar sesión', error: error.message });
   }
 };
 
 // Obtener todos los usuarios
-const getUsers = async (req, res) => {
+const getUsers = async (request, response) => {
   try {
     const users = await User.find();
-    res.status(200).json(users);
+    response.status(200).json(users);
   } catch (error) {
-    res.status(500).json({ message: 'Error al obtener los usuarios', error: error.message });
+    response.status(500).json({ message: 'Error al obtener los usuarios', error: error.message });
   }
 };
 
 // Eliminar un usuario
-const deleteUser = async (req, res) => {
+const deleteUser = async (request, response) => {
   try {
-    const { id } = req.params;
+    const { id } = request.params;
 
     // Eliminar el usuario por ID
     const deletedUser = await User.findByIdAndDelete(id);
     if (!deletedUser) {
-      return res.status(404).json({ message: 'Usuario no encontrado' });
+      return response.status(404).json({ message: 'Usuario no encontrado' });
     }
 
-    res.status(200).json({ message: 'Usuario eliminado con éxito' });
+    response.status(200).json({ message: 'Usuario eliminado con éxito' });
   } catch (error) {
-    res.status(500).json({ message: 'Error al eliminar el usuario', error: error.message });
+    response.status(500).json({ message: 'Error al eliminar el usuario', error: error.message });
   }
 };
 
 // Actualizar un usuario
-const updateUser = async (req, res) => {
+const updateUser = async (request, response) => {
   try {
-    const { id } = req.params;
-    const { username, accountActive } = req.body;
+    const { id } = request.params;
+    const { username, accountActive } = request.body;
 
     // Actualizar el usuario por ID
     const updatedUser = await User.findByIdAndUpdate(id, { username, accountActive }, { new: true });
     if (!updatedUser) {
-      return res.status(404).json({ message: 'Usuario no encontrado' });
+      return response.status(404).json({ message: 'Usuario no encontrado' });
     }
 
-    res.status(200).json({ message: 'Usuario actualizado con éxito', user: updatedUser });
+    response.status(200).json({ message: 'Usuario actualizado con éxito', user: updatedUser });
   } catch (error) {
-    res.status(500).json({ message: 'Error al actualizar el usuario', error: error.message });
+    response.status(500).json({ message: 'Error al actualizar el usuario', error: error.message });
   }
 };
 
